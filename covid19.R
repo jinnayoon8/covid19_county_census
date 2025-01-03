20c7170810387846afdaf1f097edacb349efa98f
Jinna Yoon <jinnayoon8@gmail.com>
2025-01-03 14:04:38 -0600

    modified source file

---
+++ covid19.R
@@ -0,0 +1,6 @@
+
+#write a function that will run in the shiny app
+calc_hosp = function(data){
+  data %>%
+    mutate(Percent_Hospitalized_With_Covid = (`Covid19 Hospital Admissions (per 100k)` / `Covid Cases (per 100k)`) * 100)
+}
